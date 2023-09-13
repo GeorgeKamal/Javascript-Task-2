4d8a86a27b18c3d2e98985112d23902303e211bc
GeorgeKamal <83922448+GeorgeKamal@users.noreply.github.com>
2023-09-13 18:55:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const random = Math.floor(Math.random() * 4501); // +1 to have 5000 inclusive
+const random = Math.floor(Math.random() * 4501);
 const actualNumber = 500 + random;
-document.getElementById("actual").innerText = actualNumber; // set the correct number for testing
+document.getElementById("actual").innerText = actualNumber;
 const inputNode = document.getElementById("predicted");
 
 const listener = () => {
@@ -9,13 +9,22 @@ const listener = () => {
   let text = "Success!!";
   let color = "green";
 
-  if (predictedNumber > actualNumber) {
-    text = "Too High! Try lower values";
+  if (isNaN(predictedNumber)) {
+    text = "Enter a valid number";
     color = "red";
   }
-  if (predictedNumber < actualNumber) {
-    text = "Too Low! Try higher values";
+  if (predictedNumber < 500 || predictedNumber > 5000) {
+    text = "Enter a valid Range (500-5000)";
     color = "red";
+  } else {
+    if (predictedNumber > actualNumber) {
+      text = "Too High! Try lower values";
+      color = "red";
+    }
+    if (predictedNumber < actualNumber) {
+      text = "Too Low! Try higher values";
+      color = "red";
+    }
   }
 
   statusNode.innerText = text;
